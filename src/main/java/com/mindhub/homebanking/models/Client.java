cefae152fedbaedf3da13ca3a0af3ef98b687e24
Carlap09 <cpacheco0978@gmail.com>
2023-08-04 02:26:43 -0300

    primer commit

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,58 @@
+package com.mindhub.homebanking.models;
+
+
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+
+
+public class Client {
+    @Id
+
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    private String email;
+
+    public Client() {
+    }
+
+    public Client(String id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
+
+
